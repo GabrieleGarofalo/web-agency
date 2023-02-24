cebf0ef355befde9f90361f3a1391dc9fa630949
Gabriele Garofalo <gabri.garo@outlook.it>
2023-02-24 17:38:14 +0100

    first commit

---
+++ resources/javascript/main.js
@@ -0,0 +1,28 @@
+
+    
+
+    // Fixed Navbar
+  
+
+
+    // Back to top button
+    
+
+    /*sezione carosello 2*/
+ 
+    let swiper = new Swiper(".mySwiper", {
+        effect: "flip",
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+   
+
+
+
+     
